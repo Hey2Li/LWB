d82cb24091acafc367bbed18e26ffd2cb50f1135
litao <litao@cooplan.cn>
2018-12-03 13:50:12 +0800

    上拉刷新模拟数据处理

--- LWB/View/Home/WBHomeViewController.swift
+++ LWB/View/Home/WBHomeViewController.swift
@@ -19,11 +19,18 @@ class WBHomeViewController: WBBaseViewController {
         //模拟延时加载数据-> dispatch_after
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
             for i in 0..<15 {
-                //将数据插入数组的顶部
-                self.statusList.insert(i.description, at: 0)
+                if self.isPullup {
+                    //将数据追加到底部
+                    self.statusList.append("上拉：\(i)")
+                } else {
+                    //将数据插入数组的顶部
+                    self.statusList.insert(i.description, at: 0)
+                }
             }
-            print("刷新表格")
+            print("加载数据结束")
             self.refreshControl?.endRefreshing()
+            //恢复上拉刷新标记
+            self.isPullup = false
             self.tableView?.reloadData()
         }
     }
